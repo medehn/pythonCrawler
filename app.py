eb05204b5ce42bd16a6859d9a280a8c6150c61d5
medehn <medehn85@gmail.com>
2019-09-05 07:37:21 +0200

    first stage

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, redirect, url_for
+from flask import render_template
+import requests
+import re
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def getLinks():
+    if request.method == "POST":
+        inputUrl = request.form['inputUrl']
+
+        siteRequest = requests.get(inputUrl)
+        linkList = re.findall(r'<a[^>]* href="([^"]*)"', str(siteRequest.content))
+        internalLinks = []
+        externalLinks = linkList.copy()
+
+        for link in linkList:
+            if link.startswith(siteRequest.url):
+                newLink = link.split(siteRequest.url)
+                internalLinks.append('/'+newLink[1])
+                externalLinks.remove(link)
+
+        externalLinks.sort()
+
+        return render_template("results.html", inputUrl=inputUrl, intern = internalLinks, extern = externalLinks)
+
+    else:
+        return render_template("results.html")
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+function newSearch(){
+
+    var newLink = document.getElementById("myLink").textContent;
+    console.log(newLink);
+    console.log("Foo");
+}
